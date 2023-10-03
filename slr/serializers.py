e8c5afe45727ccfca8af4f37a3f64724f11814bb
NikhilK30 <nikhilkankanwadi@gmail.com>
2023-10-03 18:43:00 +0530

    1

---
+++ slr/serializers.py
@@ -0,0 +1,8 @@
+# slr/serializers.py
+from rest_framework import serializers
+from .models import Shop
+
+class ShopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shop
+        fields = '__all__'
