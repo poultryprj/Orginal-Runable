923f508b27d31174faf048a5eb06358593b3875d
NikhilK30 <nikhilkankanwadi@gmail.com>
2023-10-04 12:01:34 +0530

    modifies manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loginapi.settings')
@@ -14,9 +13,9 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        )from exc
-    execute_from_command_line(sys.argv)
+        ) from exc  # Fix the syntax by adding 'from exc' after 'except ImportError'
 
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
